4a002688b3f48a585b19ab0126414d89d5682785
Faust36 <jonathansfish@gmail.com>
2018-03-12 18:06:45 -0400

    Being custom audio player

--- frontend/components/playbar/playbar_show.jsx
+++ frontend/components/playbar/playbar_show.jsx
@@ -5,8 +5,11 @@ class Playbar extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      currentSong: this.props.currentSong
+      currentSong: this.props.currentSong,
+      isPlaying: this.props.playlist.length === 0 ? true : false
     }
+    this.playOrPause = this.playOrPause.bind(this)
+    this.togglePlay = this.togglePlay.bind(this)
   }
 
   componentDidMount(){
@@ -23,7 +26,7 @@ class Playbar extends React.Component {
 
   componentWillReceiveProps(nextProps){
     if(this.state.currentSong !== nextProps.currentSong){
-      this.setState({currentSong:nextProps.currentSong})
+      this.setState({currentSong:nextProps.currentSong, isPlaying: true})
     }
   }
   // shouldComponentUpdate(nextProps, nextState){
@@ -31,14 +34,40 @@ class Playbar extends React.Component {
   //   return this.props.currentSong !== nextProps.currentSong
   // }
 
+  increaseVol(){
+
+  }
+
+  playOrPause(){
+    if(this.state.isPlaying === false){
+      return(<button className="play" onClick={this.togglePlay}>Play</button>)
+    }else{
+      return(<button className="pause" onClick={this.togglePlay}>Pause</button>)
+    }
+  }
+
+  togglePlay(){
+    if(this.state.isPlaying === false){
+      this.setState({isPlaying: true});
+      this.song.play();
+    }else{
+      this.setState({isPlaying: false});
+      this.song.pause()
+    }
+  }
+
   render(){
     if(!this.state.currentSong){
       return null
     }
-    const song = <audio src={this.state.currentSong.song_url} controls autoPlay/>
+    const song = <audio src={this.state.currentSong.song_url} ref={el => {this.song = el;}} autoPlay/>
     return(
       <footer className="main-footer">
         {song}
+        <div className="audio-player">
+          {this.playOrPause()}
+          <input type='range'/>
+        </div>
       </footer>
     )
 
