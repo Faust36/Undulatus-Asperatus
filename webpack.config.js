918ee775b889f80dd70976a371fa74f24fd82894
Faust36 <jonathansfish@gmail.com>
2018-04-04 16:45:08 -0400

    Fix entry file

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ plugins = plugins.concat(
 // include plugins config
 module.exports = {
   context: __dirname,
-  entry: "./frontend/<name of entry file>",
+  entry: "./frontend/asperatus.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
